6b275a7845d0ad0977edb390a7347d611bd2b2fa
thurtt <thurtt@gmail.com>
2023-07-03 11:05:28 -0400

    Adding the basic authentication framework

--- lib/application_tracker_web/router.ex
+++ lib/application_tracker_web/router.ex
@@ -1,6 +1,8 @@
 defmodule ApplicationTrackerWeb.Router do
   use ApplicationTrackerWeb, :router
 
+  import ApplicationTrackerWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule ApplicationTrackerWeb.Router do
     plug :put_root_layout, html: {ApplicationTrackerWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
@@ -17,7 +20,7 @@ defmodule ApplicationTrackerWeb.Router do
   scope "/", ApplicationTrackerWeb do
     pipe_through :browser
 
-    get "/", PageController, :home
+    live "/", UserLoginLive, :new
     live "/applications", ApplicationLive.Index, :index
     live "/applications/new", ApplicationLive.Index, :new
     live "/applications/:id/edit", ApplicationLive.Index, :edit
@@ -45,6 +48,44 @@ defmodule ApplicationTrackerWeb.Router do
 
       live_dashboard "/dashboard", metrics: ApplicationTrackerWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", ApplicationTrackerWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{ApplicationTrackerWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", ApplicationTrackerWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{ApplicationTrackerWeb.UserAuth, :ensure_authenticated}] do
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", ApplicationTrackerWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{ApplicationTrackerWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
     end
   end
 end
